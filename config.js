bae811a4fd7faf5cc48f4d4f7e16628d1085fb5b
HAD <103582402+hadbotz@users.noreply.github.com>
2022-06-24 14:55:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://ch
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '6285749433476': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+    name: 'Owner Had Bot/Creator HAD BOTZ',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -24,7 +24,7 @@ global.Owner = {
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['6285749433476'] // Want some help?
 global.prems = ['6285749433476'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
